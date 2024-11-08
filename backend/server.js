b78689cf4bb3b5d55549a87374f6d776356e41d4
Daniel <danieljmolina03@gmail.com>
2024-11-08 21:55:53 +0800

    debug cors

--- backend/server.js
+++ backend/server.js
@@ -56,6 +56,7 @@ app.use(cors({
     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
+      console.error("CORS error for origin:", origin);
       callback(new Error('Not allowed by CORS'));
     }
   },
