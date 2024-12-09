71e16fb7c24be6bdb8ee98aeb4d1372e713fcc8f
Daniel <danieljmolina03@gmail.com>
2024-12-10 01:44:15 +0800

    Adjust api.js

--- frontend-web-1/src/services/api.js
+++ frontend-web-1/src/services/api.js
@@ -1,19 +1,19 @@
 import axios from 'axios';
 
 // API CONFIGURATION FOR DEPLOYMENT -- UNCOMMENT ON DEPLOYMENT
-// const api = axios.create({
-//     baseURL: process.env.REACT_APP_API_URL,
-//     withCredentials: true,
-// });
+const api = axios.create({
+    baseURL: process.env.REACT_APP_API_URL,
+    withCredentials: true,
+});
 
 // API CONFIGURATION FOR DEVELOPMENT -- COMMENT ON DEPLOYMENT
-const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000',
-  withCredentials: true,
-  headers: {
-        'ngrok-skip-browser-warning': 'true',
-  },
-});
+// const api = axios.create({
+//   baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000',
+//   withCredentials: true,
+//   headers: {
+//         'ngrok-skip-browser-warning': 'true',
+//   },
+// });
 
 let alertShown = false;
 
--- frontend-web-2/src/services/api.js
+++ frontend-web-2/src/services/api.js
@@ -1,18 +1,18 @@
 import axios from 'axios';
 
 // API CONFIGURATION FOR DEPLOYMENT -- UNCOMMENT ON DEPLOYMENT
-// const api = axios.create({
-//     baseURL: process.env.REACT_APP_API_URL,
-//     withCredentials: true,
-// });
+const api = axios.create({
+    baseURL: process.env.REACT_APP_API_URL,
+    withCredentials: true,
+});
 
 // API CONFIGURATION FOR DEVELOPMENT -- COMMENT ON DEPLOYMENT
-const api = axios.create({
-    baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000',
-    withCredentials: true,
-    headers: {
-        'ngrok-skip-browser-warning': 'true', 
-    },
-});
+// const api = axios.create({
+//     baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000',
+//     withCredentials: true,
+//     headers: {
+//         'ngrok-skip-browser-warning': 'true', 
+//     },
+// });
 
 export default api;
