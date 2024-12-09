e0fd6d6d333f73eddee665ff48840aba4905c5b6
Daniel <danieljmolina03@gmail.com>
2024-12-09 20:16:21 +0800

    Responsive landing page

--- frontend-web-2/src/index.js
+++ frontend-web-2/src/index.js
@@ -4,9 +4,11 @@ import App from './App';
 import "./index.scss"
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
+const googleClientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <GoogleOAuthProvider clientId='944431468690-iited8t3bj1i37emdfvg0gdem7v0p60b.apps.googleusercontent.com'>
+  <GoogleOAuthProvider clientId={googleClientId}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
--- frontend-web-2/src/pages/landing-page/landing-page.jsx
+++ frontend-web-2/src/pages/landing-page/landing-page.jsx
@@ -1,13 +1,33 @@
 import "./landing-page.scss"
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const LandingPage = () => {
     return (
         <div className="landing-page">
-            <h1> To keep your balance, <br /> you must keep moving.<br /> </h1>
-            <p className="mt-4">
-                Welcome! This is your ultimate destination for custom bike builds and quality upgrade parts. Whether you're building from scratch or enhancing your current ride, we've got everything you need to hit the road with confidence and style. Start your biking journey with us today!
-            </p>
+            <div className="content">
+                <div className="text">
+                    <h1> To keep your balance, <br /> you must keep moving.<br /> </h1>
+                    <p className="mt-4">
+                        Welcome! This is your ultimate destination for custom bike builds and quality upgrade parts. Whether you're building from scratch or enhancing your current ride, we've got everything you need to hit the road with confidence and style. Start your biking journey with us today!
+                    </p>
+                </div>
+                <div className="btns">
+                    <button className="btn-1">
+                        <NavLink to="/bike-builder" className="nav-link">
+                            Build Your Bike
+                        </NavLink>
+                    </button>
+                    <button className="btn-2">
+                        <NavLink to="/bike-upgrader" className="nav-link">
+                            Upgrade Your Bike
+                        </NavLink>
+                    </button>
+                </div>
+            </div>
+            <div className="end">
+                <p>&copy; 2024 GearUp. All rights reserved.</p>
+            </div>
         </div>
     );
 };
